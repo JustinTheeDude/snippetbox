031e34713a156a22497c10b3fea76d728d06ed99
JustinTheeDude <JustinTheeDude@gmail.com>
2024-10-28 15:03:25 -0700

    added new templating

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,57 +1,67 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
-	"strconv"
+  "errors"
+  "fmt"
+  "net/http"
+  "strconv"
+
+  "snippetbox.justinteves.dev/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Server", "Go")
+  w.Header().Add("Server", "Go")
 
-    files := []string{
-        "./ui/html/base.html",
-        "./ui/html/partials/nav.html",
-        "./ui/html/pages/home.html",
-    }
+  snippets, err := app.snippets.Latest()
+  if err != nil {
+    app.serverError(w, r, err) 
+    return 
+  }
 
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-        app.serverError(w, r, err)
-        return
-    }
-
-    err = ts.ExecuteTemplate(w, "base", nil)
-    if err != nil {
-        app.serverError(w, r, err)
-    }
+  data := app.newTemplateData(r)
+  data.Snippets = snippets
+  
+  app.render(w, r, http.StatusOK, "home.html", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.PathValue("id"))
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
+  id, err := strconv.Atoi(r.PathValue("id"))
+  if err != nil || id < 1 {
+    http.NotFound(w, r)
+    return
+  }
+
+  snippet, err := app.snippets.Get(id)
+  if err != nil {
+    if errors.Is(err, models.ErrNoRecord) {
+      http.NotFound(w, r)
+    } else {
+      app.serverError(w, r, err)
     }
 
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+    return
+  }
+
+  data := app.newTemplateData(r)
+  data.Snippet = snippet
+
+  app.render(w, r, http.StatusOK, "view.html", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a form for creating a new snippet..."))
+  w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-    title := "O snail"
-    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-    expires := 7
+  title := "O snail"
+  content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+  expires := 7
 
-    id, err := app.snippets.Insert(title, content, expires)
-    if err != nil {
-        app.serverError(w, r, err)
-        return
-    }
+  id, err := app.snippets.Insert(title, content, expires)
+  if err != nil {
+    app.serverError(w, r, err)
+    return
+  }
 
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
